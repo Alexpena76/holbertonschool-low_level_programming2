bac0d5df3c2e26f0abd03fb2444682a35791bc14
root <root@ip-10-42-155-24.ec2.internal>
2025-06-18 12:41:59 +0000

    Numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
-* main - prints all single digit numbers of base 10 starting from 0
-*
-* Return: Always 0 (Success)
-*/
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-   int n;
+	int n;
 
-   for (n = 0; n <= 9; n++)
-   {
-   	printf("%d", n);
-   }
-   printf("\n");
-   return (0);
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
 }
