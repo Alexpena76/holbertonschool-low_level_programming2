559f65de002f1b6bc4e92f390e6481d7f4e62dad
root <root@ip-10-42-155-24.ec2.internal>
2025-06-18 13:08:54 +0000

    Numbersz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 48; n <= 57; n++)
 	{
-		printf("%d", n);
+		putchar(n);
 	}
-	printf("\n");
-return (0);
+	putchar('\n');
+	return (0);
 }
