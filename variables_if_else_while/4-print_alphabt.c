eb42974c7453318b2c1afbc8c107927add49a309
root <root@ip-10-42-155-24.ec2.internal>
2025-06-18 12:16:51 +0000

    Alphabet soup

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
-* main - prints the alphabet in lowercase, except q and e
-*
-* Return: Always 0 (Success)
-*/
+ * main - prints the alphabet in lowercase, except q and e
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-   char c;
+	char c;
 
-   for (c = 'a'; c <= 'z'; c++)
-   {
-   	if (c != 'e' && c != 'q')
-   		putchar(c);
-   }
-   putchar('\n');
-   return (0);
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
