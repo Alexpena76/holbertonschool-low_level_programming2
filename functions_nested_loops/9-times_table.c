9d27fe7d0f5593aa53d9aaba82c6d5f0788aaf6c
root <root@ip-10-42-152-38.ec2.internal>
2025-06-19 12:43:43 +0000

    Learn your times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			product = i * j;
-			
+
 			if (j == 0)
 			{
 				_putchar(product + '0');
@@ -23,7 +23,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				
+
 				if (product < 10)
 				{
 					_putchar(' ');
