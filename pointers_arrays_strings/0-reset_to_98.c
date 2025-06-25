008dbbd738f62a679df15699df77e0f59ef7f464
root <root@ip-10-42-7-190.ec2.internal>
2025-06-25 18:59:44 +0000

    98 Battery St

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the variable the pointer refers to to 98
+ * @n: pointer to an int whose value needs to be updated
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_number(int n);
+void reset_to_98(int *n);
 
 #endif
