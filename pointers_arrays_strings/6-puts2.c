040851708b79d17b1430a168a6456f771cc952e4
root <root@ip-10-42-88-49.ec2.internal>
2025-06-26 16:15:17 +0000

    Half the lies...

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: pointer to the string
+ *
+ * Return: void
+ */
 void puts2(char *str)
 {
 	int i = 0;
-	while (str[i])
+
+	while (str[i] != 0)
 	{
 		_putchar(str[i]);
 		i += 2;
