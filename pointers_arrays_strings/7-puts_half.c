9434fc01809f4498f8d29ffd41a6508b79160c5d
root <root@ip-10-42-88-49.ec2.internal>
2025-06-26 17:21:36 +0000

    Winning and fun

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: pointer to the string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int start;
+	int i;
+
+	/* Find the length of the string */
+	while (str[length] != 0)
+	{
+		length++;
+	}
+
+	/* Calculate starting position */
+	if (length % 2 == 0)
+	{
+		/* Even: print second half */
+		start = length / 2;
+	}
+	else
+	{
+		/* Odd: print last n characters where n = (length + 1) / 2 */
+		start = length - (length + 1) / 2;
+	}
+
+	/* Print from starting position to end */
+	for (i = start; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar(10);
+}
