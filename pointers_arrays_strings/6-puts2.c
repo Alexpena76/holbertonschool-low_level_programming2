d008935ba2e86640638f27d51c556f5a524c7529
root <root@ip-10-42-88-49.ec2.internal>
2025-06-26 16:34:47 +0000

    Half the lies

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,13 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != 0)
+	i = 0;
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
-		i += 2;
+		i = i + 2;
 	}
+	_putchar('\n');
 }
