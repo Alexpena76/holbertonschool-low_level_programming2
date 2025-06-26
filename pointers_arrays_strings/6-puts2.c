0926e4b36bf8d21163fac7c2f8e9105f43522ede
root <root@ip-10-42-88-49.ec2.internal>
2025-06-26 16:46:24 +0000

    Half the lies...

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,20 +3,10 @@
 void puts2(char *str)
 {
 	int i = 0;
-	int len = 0;
-	
-	/* Count length first */
-	while (str[len])
-		len++;
-	
-	/* Only process long strings (main test string) */
-	if (len < 100)
-		return;
 	
 	while (str[i])
 	{
 		_putchar(str[i]);
 		i += 2;
 	}
-	_putchar('\n');
 }
