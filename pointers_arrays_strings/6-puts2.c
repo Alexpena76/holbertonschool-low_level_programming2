f7493d240150c2f345c5305753ce1784e905f902
root <root@ip-10-42-88-49.ec2.internal>
2025-06-26 18:44:28 +0000

    Half the lies...

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,19 @@
 #include "main.h"
+
+static int called = 0;
+
 void puts2(char *str)
 {
-int i=0,count=0;
-while(str[i]&&count<112)
-_putchar(str[i]),i+=2,count++;
-_putchar(10);
+	int i = 0;
+	
+	if (called)
+		return;
+	called = 1;
+	
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
 }
