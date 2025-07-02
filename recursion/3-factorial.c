7d415db03616799b90b509e12f90fcb6e8b94bbf
root <root@ip-10-42-129-143.ec2.internal>
2025-07-02 21:00:05 +0000

    ...dream a little bigger...

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -13,13 +13,13 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	
+
 	/* Base case: factorial of 0 is 1 */
 	if (n == 0)
 	{
 		return (1);
 	}
-	
+
 	/* Recursive case: n! = n * (n-1)! */
 	return (n * factorial(n - 1));
 }
