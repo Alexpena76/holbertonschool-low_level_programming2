ce944eb8fc1872a213be7f71af6fe2acbcf91efd
root <root@ip-10-42-129-143.ec2.internal>
2025-07-02 19:57:54 +0000

    Dreams feel real...

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	/* Base case: if we reach end of string */
+	if (*s == 0)
+	{
+		return (0);
+	}
+	
+	/* Recursive case: 1 + length of remaining string */
+	return (1 + _strlen_recursion(s + 1));
+}
