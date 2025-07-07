23ef2a3c933a3763899560ec8eba67d2398ab9a4
root <root@ip-10-42-216-56.ec2.internal>
2025-07-07 15:05:20 +0000

    Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - represents a dog
+ * @name: name of the dog
+ * @age: age of the dog in years
+ * @owner: owner of the dog
+ *
+ * Description: This structure stores information about a dog
+ * including its name, age, and owner details.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
