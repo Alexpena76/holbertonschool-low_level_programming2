cb6c756087dec80b6a9cd663ecd3a94525aefb17
Alexpena76 <alexpenasilva@gmail.com>
2025-07-14 17:19:25 -0500

    3

---
+++ variables_if_else_while 2/2.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void){
+
+    char i;
+
+    for(i = 'a'; i <= 'z'; i++){
+        putchar(i);
+    }
+    putchar('\n');
+
+    return(0);
+}
