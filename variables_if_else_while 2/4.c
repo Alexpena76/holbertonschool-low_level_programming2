c3758ba61df2f88cc60c2e8e1f6a320e29ad8678
Alexpena76 <alexpenasilva@gmail.com>
2025-07-15 08:07:04 -0500

    4

---
+++ variables_if_else_while 2/4.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void){
+
+    char i;
+
+    for(i = 'a'; i <= 'z'; i++){
+        if(i != 'e' && i != 'q'){
+            putchar(i);
+        }
+    }
+    putchar('\n');
+
+    return(0);
+}
